581d55b434ac630c4f63c48545c05061e1d930f9
ay701 <ay701@nyu.edu>
2016-09-18 18:16:56 -0400

    add

--- checkRotate.py
+++ checkRotate.py
@@ -11,11 +11,16 @@ def isRotate(a,b):
 
     cnt = 0
 
+    print c,b
     for i in range(len(c)):   
+        print c[i], b[cnt]
         if c[i]==b[cnt]:
             cnt+=1
+            print cnt
         else:
             cnt=0
+            if c[i]==b[cnt]:
+                cnt=1
 
         if cnt==len(b):
             return True
@@ -24,3 +29,10 @@ def isRotate(a,b):
 
 print isRotate("apple","pplea")
 print isRotate("apple","pleap")
+
+def isrotation(s1,s2):
+     print 2*s1, s2
+     return len(s1)==len(s2) and s2 in 2*s1
+
+print isrotation("apple","pplea")
+print isrotation("apple","pleap")
