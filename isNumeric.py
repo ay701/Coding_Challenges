c2bce4e138f30b21c88b788945ce0f9194a85ffc
ay701 <ay701@nyu.edu>
2016-09-19 19:55:43 -0400

    add

---
+++ isNumeric.py
@@ -0,0 +1,31 @@
+def isNumeric(data):
+    point = False
+
+    if data[0]=="-":
+        data = data[1:]
+
+    for i in range(len(data)):
+   
+        if data[i] not in [".","0","1","2","3","4","5","6","7","8","9"]:
+            print data
+            return False
+
+        # point cannot be at start of string
+        if data[i]=="." and i==0:
+            return False
+
+        # point cannot be at end of string
+        if data[i]=="." and i==len(data)-1:
+            return False
+
+        # string cannot have mulitiple points
+        if point and data[i]==".":
+            return False
+
+        if data[i]==".":
+            point = True
+
+    return True
+
+print isNumeric("-3.49")
+print isNumeric("-.49")
