0a5b2d161b9db5db2637acc47123909277d3c35e
ay701 <ay701@nyu.edu>
2016-09-20 02:25:10 -0400

    add

---
+++ backForwardBrowser.py
@@ -0,0 +1,46 @@
+#Simplest solution would be to use two stacks (back, forward).
+#Clicking the back button will pop from the back stack and push the current page on the forward stack (and go to the popped value on the back stack).
+#Clicking on any link on a page (following a link) will clear the forward stack and push the current page on the back stack.
+#Clicking on the forward button will pop from the forward stack and push the current page on the back stack (and go to the popped value on the forward stack).
+#The back and forward buttons are disabled when the appropriate stack is empty.
+
+class Stack:
+
+    stack = []
+
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return len(self.stack)==0
+
+    def pop(self):
+        return self.stack.pop()
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def top(self):
+        return self.stack[-1]
+
+class Browser:
+
+    def __init__(self):
+        self.f_s = Stack()
+        self.b_s = Stack()
+        self.cur_url = "Blank"
+
+    def click(self,url):
+        self.f_s = Stack()
+        self.b_s.push(self.cur_url)
+        self.cur_url = url
+
+    def forward(self):
+        self.b_s.push(self.cur_url)
+        self.cur_url = self.f_s.pop()
+
+    def backward(self):
+        self.f_s.push(self.cur_url)
+        self.cur_url = self.b_s.pop()
+
+
