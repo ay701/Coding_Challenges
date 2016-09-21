4e2fc5ff0303460ff3628aa96d7a9a624875e1cc
ay701 <ay701@nyu.edu>
2016-09-21 15:02:26 -0400

    add

--- findPeakElement.py
+++ findPeakElement.py
@@ -1,5 +1,6 @@
 # @param num, a list of integer
 # @return an integer
+# this only find one peak number
 
 def findPeakElement(nums):
     start = 0
@@ -7,7 +8,6 @@ def findPeakElement(nums):
 
     while start+1 < end:
         mid = (start+end)//2
-        print mid
         if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
             return mid
             
@@ -19,6 +19,6 @@ def findPeakElement(nums):
     return start if nums[start]>=nums[end] else end
 
 # return 1
-print findPeakElement([2,3,1])
+print findPeakElement([2,3,17,5,16,8,1])
 # return 0
 print findPeakElement([4,3,1])
