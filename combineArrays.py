cf2361f3f1dd0794a819d171a9cd4aa3f4474ea1
ay701 <ay701@nyu.edu>
2016-09-28 16:30:33 -0400

    add

--- combineArrays.py
+++ combineArrays.py
@@ -53,4 +53,26 @@ def mergeSort(L):
 L1 = [1,4,5,6,10,5,8]
 L2 = [1,14,3,16,11,15,8]
 
-print combineArrays(L1,L2)
+#print combineArrays(L1,L2)
+
+
+# Use list comprehension
+# But this will not remove duplicates from individual list
+def combineArrays1(L1, L2):
+
+    output = L1
+    output.extend([x for x in L2 if x not in output])
+    return output
+
+print combineArrays1(L1,L2)
+
+# Use set
+def combineArrays2(L1, L2):
+
+    s1 = set(L1)
+    s2 = set(L2)
+
+    return list(s2) + list(s1 - s2)
+
+print combineArrays2(L1,L2)
+
