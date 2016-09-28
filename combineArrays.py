4ee28538fa4f60d64d309d1bb8146eb288ebbcf8
ay701 <ay701@nyu.edu>
2016-09-28 16:16:36 -0400

    add

--- combineArrays.py
+++ combineArrays.py
@@ -2,7 +2,10 @@ def combineArrays(L1, L2):
   
     output = []
     L = L1 + L2
-    L.sort()
+    # L.sort()
+    L = mergeSort(L)
+    print L
+    exit()
     
     if len(L)<=1:
     	return L
@@ -17,6 +20,34 @@ def combineArrays(L1, L2):
         cur = nex        
         output.append(cur)
 
+    return output
+
+def mergeSort(L):
+
+    output = []
+
+    if len(L)<2:
+    	return L
+
+    mid = int(len(L)/2)
+    left = mergeSort(L[:mid])
+    right = mergeSort(L[mid:])
+
+    mergeSort(left)
+    mergeSort(right)
+    
+    i = 0
+    j = 0
+
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+    	    output.append(left[i])
+            i += 1
+        else:
+    	    output.append(right[j])
+            j += 1
+
+    output += left[i:] + right[j:]
     return output
 
 L1 = [1,4,5,6,10,5,8]
