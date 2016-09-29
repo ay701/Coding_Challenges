719b177ecb07607d0af38f9318c1f00b5c8150f5
ay701 <ay701@nyu.edu>
2016-09-29 04:17:25 -0400

    add

--- find_num_duplicates.py
+++ find_num_duplicates.py
@@ -13,28 +13,32 @@
 """
 
 def find_num_duplicates(s):
+    
     output = ""
-    cnt = 0
-    ln = len(s)
 
-    if ln<=1:
+    if len(s)<2:
         return s
 
-    for i in range(ln-1):
+    cur = s[0]
+    cnt = 1
+
+    for i, nex in enumerate(s[1:]):
         
-        cur = s[i]
-        nex = s[i+1]
-        cnt += 1
-        print cur, nex, cnt
+        # print cur, nex, cnt
         
         if cur!=nex:
             output += cur if cnt==1 else cur + str(cnt)
-            cnt = 0
+            cnt = 1
+        else:
+            cnt += 1
+        
+        cur = nex
 
-    output += nex if cur!=nex else cur + str(cnt+1)
+    output += nex if s[-2]!=nex else nex + str(cnt)
 
     return output       
     
 print find_num_duplicates("hi")
 print find_num_duplicates("aaabaaa")
 print find_num_duplicates("aaaabbcddd")
+print find_num_duplicates("aaaabbcd")
