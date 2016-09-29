b4471ea23cfb37fbadcfc949884f04ab2f3f22fd
ay701 <ay701@nyu.edu>
2016-09-29 17:00:12 -0400

    add

--- addTwoLinkedlist.py
+++ addTwoLinkedlist.py
@@ -31,10 +31,18 @@ def addTwoLinkedlist(head1, head2):
         cur = cur.next
  
     if head1:
-        cur.next = head1
+    	while head1:
+            cur.next = Node( int((head1.data + flag)%10 ) )
+            flag = int((head1.data + flag)/10)
+            head1 = head1.next
+            cur = cur.next
 
     if head2:
-    	cur.next = head2
+    	while head2:
+            cur.next = Node( int((head2.data + flag)%10 ) )
+            flag = int((head2.data + flag)/10)
+            head2 = head2.next
+            cur = cur.next
 
     return fake.next
 
