aa09574af41b5eae45358116792d5c5865e1eb1d
ay701 <ay701@nyu.edu>
2016-10-09 17:07:49 -0400

    add

---
+++ reverseWordsInString.py
@@ -0,0 +1,32 @@
+def reverseWordsInString(data):
+ 
+    output = []
+    data = reverse(data)
+    prev = -1
+
+    for cur, item in enumerate(data):
+        if item == " ":
+            prev += 1
+            output.append(reverse(data[prev:cur]))
+            output.append(" ")
+            prev = cur
+
+    output.append(reverse(data[prev+1:cur+1]))
+
+    return "".join(output)
+
+
+def reverse(data):
+
+    data = list(data)
+    leng = len(data)
+
+    for i in range(leng):
+        if i<=leng-i-1:
+            tmp = data[leng-i-1]
+            data[leng-i-1] = data[i]
+            data[i] = tmp
+
+    return ''.join(data)
+
+print reverseWordsInString(" hi nihao esmayil")
