f51f9bacf9368d7d578e840a1628ad39fd8179b8
ay701 <ay701@nyu.edu>
2016-10-10 00:16:22 -0400

    add

--- strToint.py
+++ strToint.py
@@ -4,18 +4,18 @@ def strToInt(st):
     neg = False
     ret = 0
 
+    if st[0]=="-":
+        neg = True
+        st = st[1:]
+
+    if len(st)>1 and st[0]=="0":
+        exit("Invalid")
+
     for i in range(len(st)):
 
         asc = ord(st[i])
 
-        if i==0 and st[i]=="-":
-            neg = True
-            continue
-
-        if i==0 and asc==48 and len(st)>1:
-            exit("Invalid")
-
-        if i and asc<48 or asc>72:
+        if asc<48 or asc>72:
             exit("Invalid")
 
         ret *= 10
