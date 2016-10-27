921c8c1aca8a5eb938a086c1f8d9e51c556ac8ac
ay701 <ay701@nyu.edu>
2016-10-27 15:53:09 -0400

    add

--- gridConnected.py
+++ gridConnected.py
@@ -21,11 +21,10 @@ def ifConnected(grid):
         # Returns a list of cells that are adjacent.
         candidates = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
         return [c for c in candidates if c[0] in range(n) and c[1] in range(n) and grid[c[0]][c[1]]==1]
-
-    # calculate total non-empty cells
-    cnt = 0
-    cells = []
-    checked_cells = [] 
+    
+    cnt = 0 # Total non-empty cells
+    cells = [] # DFS stack
+    checked_cells = [] # Checked cells
 
     for x, row in enumerate(grid):
         for y, col in enumerate(row):
@@ -36,12 +35,11 @@ def ifConnected(grid):
                     cells = [(x,y)]
     
     while len(cells)>0:
-
         cell = cells.pop(0)
         checked_cells.append(cell)
         cells.extend([x for x in neighbors(cell[0],cell[1]) if x not in checked_cells])
 
-    print cnt, len(checked_cells)
+    # print cnt, len(checked_cells)
     return cnt==len(checked_cells)
 
 # grid = [[1, 0, 1],
