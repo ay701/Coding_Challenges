2d0ed850b80ffa2c7b2bb47926d3d9ef64d2234f
ay701 <ay701@nyu.edu>
2016-11-19 00:58:02 -0500

    add

---
+++ palindromeNumber.py
@@ -0,0 +1,28 @@
+# Leetcode 9
+# Determine whether an integer is a palindrome. Do this without extra space.
+
+def isPalindrome_int(num):
+
+    if num<0:
+    	num = num*-1
+
+    div = 1
+    while num/div>10:
+    	div *= 10
+    	
+    print div
+
+    while div>1:
+    	left = num/div
+    	right = num%10
+
+        print left, right
+    	if left!=right:
+    		return False
+
+        num = (num%div)/10
+        div/=100 
+
+    return True
+
+print isPalindrome_int(12321)
