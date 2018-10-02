31dd2e5f1b6e4bc65d4b0bfb1e608f8473d1ae44
akkking <ay701@nyu.edu>
2018-10-02 14:21:48 -0400

    update

--- number/power_.py
+++ number/power_.py
@@ -8,26 +8,26 @@
 # 2,4 = 16 = power(2,2) * power(2,2)
 # 2,5 = 16 = power(2,2) * power(2,2) * 2
 
+
 def power_(m, n):
     
     neg = False
-    rtn = 0
     
-    if n<0:
+    if n < 0:
         neg = True
         n = -1*n
     
-    if n==0:
+    if n == 0:
         return 1
     
-    if n==1:
+    if n == 1:
         return m
     
     tmp = power_(m, n/2)
-    rtn = tmp * tmp if n%2==0 else tmp * tmp * m 
+    rtn = tmp * tmp if n % 2 == 0 else tmp * tmp * m
        
     return 1/float(rtn) if neg else rtn
     
-print power_(2,3)
-print power_(5,2)
-print power_(5,-2)
+print power_(2, 3)
+print power_(5, 2)
+print power_(5, -2)
--- tree/lca.py
+++ tree/lca.py
@@ -1,5 +1,6 @@
 # Lowest Common Ancestor
 # http://www.crazyforcode.com/lowest-common-ancestor-binary-tree/
+# http://www.crazyforcode.com/lowest-common-ancestor-binary-tree/
 
 def lca(root,x,y):
     if root is None or root==x or root==y:
@@ -20,10 +21,36 @@ def lca(root,x,y):
     return None
 
 
-def lca_2(root,x,y):
-    
-    if root.data == x.data
+# A recursive python program to find LCA of two nodes
+# n1 and n2
 
+# A Binary tree node
+class Node:
+    # Constructor to create a new node
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+# Function to find LCA of n1 and n2. The function assumes
+# that both n1 and n2 are present in BST
+def lca(root, n1, n2):
+    # Base Case
+    if root is None:
+        return None
+
+    # If both n1 and n2 are smaller than root, then LCA
+    # lies in left
+    if (root.data > n1 and root.data > n2):
+        return lca(root.left, n1, n2)
+
+        # If both n1 and n2 are greater than root, then LCA
+    # lies in right
+    if (root.data < n1 and root.data < n2):
+        return lca(root.right, n1, n2)
+
+    return root
 
 def findPath(root,x):
     path = []
