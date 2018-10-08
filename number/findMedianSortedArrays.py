0aaef52203e3129bdbb8613cb4f155d5052aed76
akkking <ay701@nyu.edu>
2018-10-08 16:56:05 -0400

    update

--- number/findMedianSortedArrays.py
+++ number/findMedianSortedArrays.py
@@ -6,37 +6,38 @@
 # We get { 10, 11, 12, 15, 20 } after sorting. Median is the middle element of the sorted array which is 12.
 # Time complexity Log(n)
 
-def findMedianSortedArrays(L1,L2):
 
-    m1 = len(L1)/2
-    m2 = len(L2)/2
+def find_median_sorted_arrays(l1, l2):
 
-    print m1,m2
+    m1 = len(l1)/2
+    m2 = len(l2)/2
 
-    if len(L1)==0 :
-    	return m2
+    print m1, m2
 
-    if len(L2)==0 :
-    	return m1
+    if len(l1) == 0:
+        return l2[m2]
 
-    if L1[m1]==L2[m2]:
-    	return L1[m1]
+    if len(l2) == 0:
+        return l1[m1]
 
-    if len(L1)==2 and len(L2)==2:
-    	return (max(L1[0],L2[0])+min(L1[1],L2[1]))/2
+    if l1[m1] == l2[m2]:
+        return l1[m1]
 
-    if L1[m1]>L2[m2]:
-    	small = L2[m2:len(L2)]
-    	big = L1[0:m1+1]
-    	#print small, big
-    	return findMedianSortedArrays(small,big)
-    elif L1[m1]<L2[m2]:
-        small = L1[m1:len(L1)]
-        big = L2[0:m2+1]
-    	# print small, big
-    	return findMedianSortedArrays(small,big)
+    if len(l1) == 2 and len(l2) == 2:
+        return (max(l1[0], l2[0]) + min(l1[1], l2[1]))/2
 
-print findMedianSortedArrays([1, 12, 15, 26, 38],[2, 13, 17, 30, 45])
+    if l1[m1] > l2[m2]:
+        small = l2[m2:len(l2)]
+        big = l1[0:m1+1]
+        #print small, big
+        return find_median_sorted_arrays(small, big)
+    elif l1[m1] < l2[m2]:
+        small = l1[m1:len(l1)]
+        big = l2[0:m2+1]
+        # print small, big
+        return find_median_sorted_arrays(small, big)
+
+print find_median_sorted_arrays([1, 12, 15, 26, 38], [2, 13, 17, 30, 45])
 
 
 
