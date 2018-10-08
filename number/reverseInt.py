c1725552101c7e0858f492af6164d454129244a2
akkking <ay701@nyu.edu>
2018-10-08 11:18:12 -0400

    update

--- number/reverseInt.py
+++ number/reverseInt.py
@@ -1,23 +1,24 @@
-# Reverve Integer
+# Reverse Integer
 # Consider negative number
 # Consider the number end with 0
 
-def reverseInt(data):
+
+def reverse_int(data):
 
     output = 0
     neg = False
 
-    if data<0:
-    	neg = True
-    	data = -1*data
+    if data < 0:
+        neg = True
+        data = -1 * data
 
-    while data>0:
-        remd = data%10
-        output = output*10 + remd
-        data = data/10
-        # print data, remd, output 
+    while data > 0:
+        rem = data % 10
+        output = output * 10 + rem
+        data /= 10
+        # print data, rem, output
         
     return output if not neg else -1*output
 
-print reverseInt(456730)
-# print reverseInt(53421.23)
+print reverse_int(456730)
+# print reverse_int(53421.23)
