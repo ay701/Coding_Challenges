0ca2a543c9430acfb4df88b4fd7e849ca241212d
akkking <ay701@nyu.edu>
2018-10-08 22:24:25 -0400

    update

---
+++ tree/findMinimumDepathBinaryTree.py
@@ -0,0 +1,55 @@
+# Input:
+#
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+#   /
+#  14
+#
+# Output: 2
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def min_depth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        depth, curr_level = 0, [root]
+
+        while curr_level:
+            depth += 1
+            next_level = []
+
+            for n in curr_level:
+                left, right = n.left, n.right
+                if left is None and right is None:
+                    return depth
+
+                if left:
+                    next_level.append(left)
+
+                if right:
+                    next_level.append(right)
+
+            curr_level = next_level
+
+        return depth
+
+
+if __name__ == "__main__":
+    None
+
