44bbd911e8271fb1534924bba624728556551dba
akkking <ay701@nyu.edu>
2018-10-11 21:59:54 -0400

    updatec

--- linkedlist/addTwoLinkedlist.py
+++ linkedlist/addTwoLinkedlist.py
@@ -5,57 +5,55 @@
 # Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 # Output: 7 -> 0 -> 8
 
-class Node:
-    def __init__(self, data=None, nex=None):
-    	self.data = data
-    	self.next = nex
 
-def addTwoLinkedlist(head1, head2):
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+def add_two_linked_list(head1, head2):
 
     if head1 is None:
-    	return head2
+        return head2
 
     if head2 is None:
-    	return head1
+        return head1
 
     flag = 0
     fake = Node(0)
     cur = fake
 
     while head1 and head2:
-        
-        cur.next = Node(int((head1.data + head2.data + flag)%10))
+        cur.next = Node(int((head1.data + head2.data + flag) % 10))
         flag = int((head1.data + head2.data + flag)/10)
         head1 = head1.next
         head2 = head2.next
         cur = cur.next
- 
-    if head1:
-    	while head1:
-            cur.next = Node( int((head1.data + flag)%10 ) )
-            flag = int((head1.data + flag)/10)
-            head1 = head1.next
-            cur = cur.next
 
-    if head2:
-    	while head2:
-            cur.next = Node( int((head2.data + flag)%10 ) )
-            flag = int((head2.data + flag)/10)
-            head2 = head2.next
-            cur = cur.next
+    while head1:
+        cur.next = Node(int((head1.data + flag) % 10))
+        flag = int((head1.data + flag)/10)
+        head1 = head1.next
+        cur = cur.next
+
+    while head2:
+        cur.next = Node(int((head2.data + flag) % 10))
+        flag = int((head2.data + flag)/10)
+        head2 = head2.next
+        cur = cur.next
 
     if flag == 1:
-    	cur.next = Node(1)
+        cur.next = Node(1)
 
     return fake.next
 
 
-head1 = Node(2,Node(4,Node(3)))
-head2 = Node(5,Node(6,Node(4)))
-head = addTwoLinkedlist(head1,head2)
+head1 = Node(2, Node(4, Node(3)))
+head2 = Node(5, Node(6, Node(4)))
+head = add_two_linked_list(head1, head2)
+
 
 while head:
-	print head.data
-	head = head.next
-
-
+    print head.data
+    head = head.next
