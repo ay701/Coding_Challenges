ce80f05b48d48e8754ce8b8b93714a66a7dd251e
akkking <ay701@nyu.edu>
2018-10-24 10:30:50 -0400

    update

--- number/firstMissingPositive.py
+++ number/firstMissingPositive.py
@@ -7,6 +7,8 @@
 # Thoughts:
 # Swap the number to make l[i]=i+1, scan the list and return first l[i] which is not i+1
 
+import collections
+
 
 def first_missing_positive(l):
 
@@ -16,7 +18,9 @@ def first_missing_positive(l):
         return 1
 
     for i in range(n):
-        if l[i] in range(1, n+1) and l[i] != i+1 and l[l[i]-1] != l[i]:
+        if l[i] in range(1, n+1) \
+                and l[i] != i+1 \
+                and l[l[i]-1] != l[i]:
             tmp = l[l[i]-1]
             l[l[i]-1] = l[i]
             l[i] = tmp
@@ -25,8 +29,31 @@ def first_missing_positive(l):
         if l[i] != i+1:
             return i+1
 
-    return l[-1]+1
+    return l[-1]+1  # if not found, return last element
 
 l = [2, 1, 3, 9, 5]
 
 print first_missing_positive(l)
+
+######
+# https://codereview.stackexchange.com/questions/149270/given-an-unsorted-integer-array-find-the-first-missing-positive-integer
+######
+
+def first_missing_positive_2(l):
+
+    dic = collections.defaultdict(int)
+    n = len(l)
+
+    for e in l:
+        dic[e] = 1
+
+    for i in range(1, n+1):
+        if dic.get(i) == 0:
+            return i
+
+    return n+1
+
+l2 = [2, 1, 3, 9, 5]
+
+print first_missing_positive(l2)
+
