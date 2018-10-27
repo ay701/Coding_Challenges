57979f54ff2d4c09b9bf0a65805dcb22fc9d66ea
akkking <ay701@nyu.edu>
2018-10-26 23:50:54 -0400

    update

--- graph/boggle.py
+++ graph/boggle.py
@@ -18,7 +18,69 @@
 #
 # https://www.geeksforgeeks.org/boggle-find-possible-words-board-characters/
 # http://www.martinbroadhurst.com/boggle-in-python.html
+# 1. Use DFS
+# 2. Use Trie
 
-def boggle():
+class TrieNode:
+
+    def __init__(self, char=""):
+        self.char = char
+        self.children = []
+        self.word_finished = False # is it last char of word
+        self.counter = 1 # how many times this char appeared in addition process
 
 
+def add_word(root, word):
+
+    node = root
+
+    for ch in word:
+        # We only care if its found in children
+        # update node with new node for loop
+        found_in_child = False
+
+        for child in node.children:
+            if child.char == ch:
+                child.counter += 1
+                found_in_child = True
+                node = child
+                break
+
+        if not found_in_child:
+            new_node = TrieNode(ch)
+            node.children.append(new_node)
+            node = new_node
+
+    node.word_finished = True
+
+
+def boggle(root, word):
+
+    node = root
+
+    for ch in word:
+
+        found_in_child = False
+
+        for child in node.children:
+
+            if child.char == ch:
+                found_in_child = True
+                node = child
+                break
+
+        # Not found
+        if not found_in_child:
+            return False, 0
+
+    return True, node.counter
+
+
+dictionary = ["GEEKS", "FOR", "QUIZ", "GO"]
+
+root = TrieNode()
+for word in dictionary:
+    add_word(root, word)
+
+
+print(boggle(root, "FOR"))
