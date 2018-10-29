5bedcf3c5b02d18c04bcb27753059f231d35054c
akkking <ay701@nyu.edu>
2018-10-29 11:58:22 -0400

    update

--- string/wordLadder.py
+++ string/wordLadder.py
@@ -1,4 +1,5 @@
-# Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, 
+# Given two words (start and end), and a dictionary,
+# find the length of shortest transformation sequence from start to end,
 # such that only one letter can be changed at a time and each intermediate word must exist in the dictionary. 
 # For example, given:
 # start = "hit"
@@ -6,31 +7,35 @@
 # dict = ["hot","dot","dog","lot","log"]
 # One shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog", the program should return its length 5.
 
-def wordLadder(start,end,dic):
+# Use BFS
+# https://www.programcreek.com/2012/12/leetcode-word-ladder/
 
-    if len(start)!=len(end):
-    	return 0
 
-    leng = len(start)
-    queque = [(start,1)]  # add start to queque with level 1
+def word_ladder(start, end, word_list):
 
-    while len(queque)>0:
-    	word_tuple = queque.pop(0)
+    if len(start) != len(end):
+        return 0
+
+    n = len(start)
+    queue = [(start, 1)]  # add start to queue with level 1
+
+    while len(queue) > 0:
+        word_tuple = queue.pop(0)
         word = word_tuple[0]
         level = word_tuple[1]
 
         for i, c in enumerate(word):
-        
-            for c_ in range(97,123):
-                if c==chr(c_):
-            	    continue
+            # Ascii code from "a" to "z"
+            for code in range(ord('a'), ord('z')+1):
+                if c == chr(code):
+                    continue
 
-                tmp_word = word[0:i] + chr(c_) + word[i+1:leng]
-                if tmp_word==end:
-                	return level+1
+                tmp_word = word[0:i] + chr(code) + word[i+1:n]
+                if tmp_word == end:
+                    return level+1
 
-                if tmp_word in dic:
-            	    queque.append((tmp_word,level+1))
+                if tmp_word in word_list:
+                    queue.append((tmp_word, level+1))
 
     return 0
 
@@ -38,5 +43,6 @@ def wordLadder(start,end,dic):
 start = "hit"
 end = "cog"
 # end = "hot"
-dic = ["hot","dot","dog","lot","log"]
-print wordLadder(start,end,dic)
+word_list = ["hot", "dot", "dog", "lot", "log"]
+
+print word_ladder(start, end, word_list)
