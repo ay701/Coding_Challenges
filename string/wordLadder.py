39beb4e82c5f5f0aa3f732ab3e3e069af029adf3
akkking <ay701@nyu.edu>
2018-10-29 13:27:32 -0400

    update

--- string/wordLadder.py
+++ string/wordLadder.py
@@ -20,7 +20,7 @@ def word_ladder(start, end, word_list):
     queue = [(start, 1)]  # add start to queue with level 1
 
     while len(queue) > 0:
-        word_tuple = queue.pop(0)
+        word_tuple = queue.pop(0)  # BFS, pop the earliest word
         word = word_tuple[0]
         level = word_tuple[1]
 
@@ -46,3 +46,66 @@ end = "cog"
 word_list = ["hot", "dot", "dog", "lot", "log"]
 
 print word_ladder(start, end, word_list)
+
+
+# LeetCode - Word Ladder II
+# Given two words (start and end), and a dictionary, find all shortest transformation sequence(s) from start to end,
+# such that: 1) Only one letter can be changed at a time, 2) Each intermediate word must exist in the dictionary.
+#
+# For example, given: start = "hit", end = "cog", and dict = ["hot","dot","dog","lot","log"], return:
+#
+# [
+#     ["hit", "hot", "dot", "dog", "cog"],
+#     ["hit", "hot", "lot", "log", "cog"]
+# ]
+
+
+def word_ladder_2(start, end, word_list):
+
+    if len(start) != len(end):
+        return 0
+
+    n = len(start)
+    queue = [[start, [start]]]  # add start to queue
+    output = []
+
+    while len(queue) > 0:
+
+        if len(output) and len(output[0]) in range(1, len(queue[-1][1])):
+            return output
+
+        # if len(output) > 0:
+        #     print(output)
+        #     exit(0)
+
+        word_tuple = queue.pop(0)  # BFS, pop the earliest word
+        word = word_tuple[0]
+        steps = word_tuple[1]
+
+        for i, c in enumerate(word):
+            # Ascii code from "a" to "z"
+            for code in range(ord('a'), ord('z')+1):
+                if c == chr(code):
+                    continue
+
+                tmp_word = word[0:i] + chr(code) + word[i+1:n]
+
+                if tmp_word == end:
+                    # steps.append(tmp_word)
+                    output.append(steps)
+                    continue
+
+                if tmp_word in word_list and tmp_word not in steps:
+                    steps.append(tmp_word)
+                    queue.append((tmp_word, steps))
+
+    return output
+
+
+start = "hit"
+end = "cog"
+# end = "hot"
+word_list = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+print word_ladder_2(start, end, word_list)
+
