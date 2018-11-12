1a6cf9acb11abd6bc3b6d7a04fbff66247aee962
akkking <ay701@nyu.edu>
2018-11-12 11:32:33 -0500

    update

--- tree/printBST.py
+++ tree/printBST.py
@@ -1,21 +1,27 @@
 # Print left-most node of each level
 
+
 class Node:
     def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
 
-def traverse(rootnode):
 
-    cur_level = [rootnode]
+def traverse(root):
+
+    cur_level = [root]
 
     while cur_level:
         next_level = []
         for node in cur_level:
             print node.value
-            if node.left : next_level.append(node.left)
-            if node.right : next_level.append(node.right)
+            if node.left:
+                next_level.append(node.left)
+
+            if node.right:
+                next_level.append(node.right)
+
         cur_level = next_level
        
 
@@ -27,26 +33,35 @@ def traverse_bfs(rootnode):
     while queue:
         node = queue.pop(0)
         print node.value
-        #if node.value==val:
+        # if node.value == val:
         #    return True
 
-        if node.left: queue.append(node.left)
-        if node.right: queue.append(node.right)
-    #return False
-  
+        if node.left:
+            queue.append(node.left)
+
+        if node.right:
+            queue.append(node.right)
+
+    # return False
+
+
 # Use stack, depth-first search, find node
-def traverse_dfs(rootnode):
-    queue = [rootnode]
+def traverse_dfs(root):
+    queue = [root]
 
     while queue:
         node = queue.pop(0)
         print node.value
-        #if node.value==val:
+        # if node.value == val:
         #    return True
 
-        if node.right: queue = [node.right] + queue
-        if node.left: queue = [node.left] + queue
-  
+        if node.right:
+            queue = [node.right] + queue
+
+        if node.left:
+            queue = [node.left] + queue
+
+
 t = Node(1, Node(2, Node(4, Node(7))), Node(3, Node(5), Node(6)))
 traverse(t)
 traverse_bfs(t)
