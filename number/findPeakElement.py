c9f6c0e9699092e4caaae0081297b7786a285bf9
akkking <ay701@nyu.edu>
2018-11-26 01:16:46 -0500

    update

--- number/findPeakElement.py
+++ number/findPeakElement.py
@@ -18,102 +18,29 @@
 # this is only to find one peak number
 
 
-class Solution:
+def find_peak_element(nums):
 
-    def __init__(self):
-        self.peaks = []
+    n = len(nums)
 
-    def find_peak_element(self, nums):
+    if n == 1:
+        return nums
 
-        n = len(nums)
+    left = 0
+    right = n-1
 
-        if n == 1:
-            self.peaks.append(nums[0])
-        elif n == 2:
-            self.peaks.append(nums[0] if nums[0] > nums[1] else nums[1])
-        else:
-            start = 0
-            end = n
-            mid = (start + end) // 2
+    while left < right:
+        mid = left + (right-left)//2
 
-            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
-                self.peaks.append(nums[mid])
+        if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+            return nums[mid]
+        elif nums[mid] < nums[mid-1]:
+            right = mid
+        elif nums[mid] < nums[mid+1]:
+            left = mid
 
-            print(nums[start:mid])
-            print(nums[mid:end])
-
-            self.find_peak_element(nums[start:mid])
-            self.find_peak_element(nums[mid:end])
-
-        return self.peaks
-
-        #
-        # while start < end:
-        #     mid = (start+end)//2
-        #
-        #     if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
-        #         self.peaks.append(nums[mid])
-        #
-        #     if nums[mid] < nums[mid+1]:
-        #         start = mid
-        #     else:
-        #         end = mid
-        #
-        # return nums[start] if nums[start] >= nums[end] else nums[end]
-
-        # This applies for one peak number only
-        def findPeakElement(self, nums):
-            """
-            :type nums: List[int]
-            :rtype: int
-            """
-            left, right = 0, len(nums) - 1
-            while left < right:
-                mid = (right + left) // 2
-
-                if nums[mid] < nums[mid + 1]:
-                    left = mid + 1
-                else:
-                    right = mid
-            return left
+    return nums[mid]
 
 
-print Solution().findPeakElement([2, 3, 17, 5, 16, 8, 1])
-# print Solution().find_peak_element([4, 3, 1])
-# print Solution().find_peak_element([1, 3, 20, 4, 1, 0])
-
-# def findPeakElements(nums):
-#     peaks = []
-
-#     length = len(nums)
-#     start = 0
-#     end = length-1
-
-#     # if length==1:
-#     #     return [nums[0]]
-
-#     # if length==2:
-#     #     return [nums[0]] if nums[0]>=nums[1] else [nums[1]]
-
-#     # if length==3 and nums[1]>nums[0] and nums[1]>nums[2]:
-#     #     return [nums[1]]
-
-#     while start+1<end:
-
-#         mid = (start+end)//2
-
-#         if nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]:
-#             peaks.append(nums[mid])
-#             print mid+1, end
-#             # print nums[mid+1:end]
-#             peaks += findPeakElements(nums[start:mid+1])
-#             peaks += findPeakElements(nums[mid:end])
-
-#         if nums[mid]<nums[mid-1]:
-#             end = mid-1
-#         else:
-#             start = mid+1
-
-#     return peaks+[nums[start]] if nums[start]>=nums[end] else peaks+[nums[end]]
-
-# print findPeakElements([2,3,17,5,16,8])
+print find_peak_element([2, 3, 17, 5, 16, 8, 1])
+print find_peak_element([4, 3, 1])
+print find_peak_element([1, 3, 20, 4, 1, 0])
