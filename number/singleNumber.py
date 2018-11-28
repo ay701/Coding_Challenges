960ed300624696790e79a594db902b4f334755ac
akkking <ay701@nyu.edu>
2018-11-27 21:12:50 -0500

    update

--- number/singleNumber.py
+++ number/singleNumber.py
@@ -1,6 +1,8 @@
 # Given an array of integers, every element appears twice except for one.
 # Find that single one.
-# Input will generate : 0, 2, 6, 2, 7, 2
+# Input: 3, 3, 2, 4, 4, 5, 5
+# Middle output: 0, 2, 6, 2, 7, 2
+
 
 def single_number(l):
 
@@ -8,7 +10,7 @@ def single_number(l):
 
     for i in range(1, len(l)):
         ans ^= l[i]
-        print(ans)
+        print(l[i], ans)
 
     return ans
 
